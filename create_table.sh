de66fa12d89e908eca960fad677062fd4733ab44
Aman Thakral <a@thakral.in>
2019-09-13 01:38:06 -0700

    drop and create table

--- create_table.sh
+++ create_table.sh
@@ -1,3 +1,3 @@
-awslocal dynamodb delete-table --cli-input-json file://$1; 
-awslocal dynamodb create-table --cli-input-json file://$1;
+awslocal dynamodb delete-table --table-name $1; 
+awslocal dynamodb create-table --cli-input-json file://$2;
 
